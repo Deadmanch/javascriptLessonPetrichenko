d00d92d17f69e5b98a2295fa20097ed50bb88bbb
Danil <stepanova.nata.r@gmail.com>
2022-06-17 12:58:06 +0300

    Добавлены ответы на 11 практику вторая часть - Массивы

--- js/ObjPractice/lesson11.js
+++ js/ObjPractice/lesson11.js
@@ -20,3 +20,36 @@ console.log(city.toLowerCase());
 	}
 
 	standardizeStrings(favoriteCities);
+
+
+	// Вторая часть
+
+	const someString = 'This is some strange string';
+
+function reverse(str) {
+	if(typeof(str) !== 'string') {
+		return "Ошибка!";
+	}
+	return str.split('').reverse().join('');
+
+}
+
+reverse(someString);
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+	let str = '';
+
+	arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+	arr.forEach(function(curr) {
+		if(curr !== missingCurr) {
+			str += `${curr}\n`;
+		}
+	});
+
+	return str;
+}
+
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'EUR'));

29d4052a5f89c9a31b3fa328cfb7b788c18097c6
Danil <stepanova.nata.r@gmail.com>
2022-06-23 12:29:34 +0300

    Добавлен функционал табов в проект с фруктами

---
+++ Project/project_Food/js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+	let tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
+
+	function hideTabsContent() {
+		tabsContent.forEach((item) => {
+			item.classList.remove('show', 'fade');
+			item.classList.add('hide');
+		});
+
+		tabs.forEach((item) => {
+			item.classList.remove('tabheader__item_active');
+		});
+	}
+
+	function showTabsContent(i = 0) {
+		tabsContent[i].classList.remove('hide');
+		tabsContent[i].classList.add('show', 'fade');
+
+		tabs[i].classList.add('tabheader__item_active');
+	}
+
+	hideTabsContent();
+	showTabsContent();
+
+	tabsParent.addEventListener('click', (e) => {
+		const target = e.target;
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabsContent();
+					showTabsContent(i);
+				}
+			});
+		}
+	});
+});
